62069e327c0c671443b7353e2a2e698d013c4768
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2019-09-12 18:47:03 +0100

    Add stuff to Student POJO

--- graphql/src/main/java/com/graphql/demo/service/student/Student.java
+++ graphql/src/main/java/com/graphql/demo/service/student/Student.java
@@ -19,9 +19,13 @@ public class Student {
     @JsonCreator
     public Student(
             @JsonProperty("name") String name,
-            @JsonProperty("surname") String surname) {
+            @JsonProperty("surname") String surname,
+            @JsonProperty("degree") Degree degree,
+            @JsonProperty("tutor") Tutor tutor ) {
         this.name = name;
         this.surname = surname;
+        this.degree = degree;
+        this.tutor = tutor;
     }
 
     public String getName() {
@@ -32,17 +36,27 @@ public class Student {
         return surname;
     }
 
+    public Degree getDegree() {
+        return degree;
+    }
+
+    public Tutor getTutor() {
+        return tutor;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Student student = (Student) o;
         return Objects.equals(name, student.name) &&
-                Objects.equals(surname, student.surname);
+                Objects.equals(surname, student.surname) &&
+                Objects.equals(degree, student.degree) &&
+                Objects.equals(tutor, student.tutor);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, surname);
+        return Objects.hash(name, surname, degree, tutor);
     }
 }
